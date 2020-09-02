6e0481708aab82ba3a011e4528db946719cad41b
Masck01 <lobojujenho@gmail.com>
2020-09-02 19:47:31 -0300

    Solved SLN

---
+++ TPSPAD/ConsoleAppTp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
